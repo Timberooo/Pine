32cae6c6e68beec6728995408f5931873ea8ba9d
Timber <redwoodfox479@gmail.com>
2025-08-22 21:15:53 -0500

    Added explicit conversion operators for points

--- src/math/point.h
+++ src/math/point.h
@@ -22,6 +22,19 @@ namespace Pine
 
         inline bool operator==(const Point2D<T>& other) const;
         inline bool operator!=(const Point2D<T>& other) const;
+
+        explicit inline operator Point2D<char>() const { return Point2D<char>(x, y); }
+        explicit inline operator Point2D<unsigned char>() const { return Point2D<unsigned char>(x, y); }
+        explicit inline operator Point2D<short>() const { return Point2D<short>(x, y); }
+        explicit inline operator Point2D<unsigned short>() const { return Point2D<unsigned short>(x, y); }
+        explicit inline operator Point2D<int>() const { return Point2D<int>(x, y); }
+        explicit inline operator Point2D<unsigned int>() const { return Point2D<unsigned int>(x, y); }
+        explicit inline operator Point2D<long>() const { return Point2D<long>(x, y); }
+        explicit inline operator Point2D<unsigned long>() const { return Point2D<unsigned long>(x, y); }
+        explicit inline operator Point2D<long long>() const { return Point2D<long long>(x, y); }
+        explicit inline operator Point2D<unsigned long long>() const { return Point2D<unsigned long long>(x, y); }
+        explicit inline operator Point2D<float>() const { return Point2D<float>(x, y); }
+        explicit inline operator Point2D<double>() const { return Point2D<double>(x, y); }
     };
 
 
@@ -51,6 +64,19 @@ namespace Pine
 
         inline bool operator==(const Point3D<T>& other) const;
         inline bool operator!=(const Point3D<T>& other) const;
+
+        explicit inline operator Point3D<char>() const { return Point3D<char>(x, y, z); }
+        explicit inline operator Point3D<unsigned char>() const { return Point3D<unsigned char>(x, y, z); }
+        explicit inline operator Point3D<short>() const { return Point3D<short>(x, y, z); }
+        explicit inline operator Point3D<unsigned short>() const { return Point3D<unsigned short>(x, y, z); }
+        explicit inline operator Point3D<int>() const { return Point3D<int>(x, y, z); }
+        explicit inline operator Point3D<unsigned int>() const { return Point3D<unsigned int>(x, y, z); }
+        explicit inline operator Point3D<long>() const { return Point3D<long>(x, y, z); }
+        explicit inline operator Point3D<unsigned long>() const { return Point3D<unsigned long>(x, y, z); }
+        explicit inline operator Point3D<long long>() const { return Point3D<long long>(x, y, z); }
+        explicit inline operator Point3D<unsigned long long>() const { return Point3D<unsigned long long>(x, y, z); }
+        explicit inline operator Point3D<float>() const { return Point3D<float>(x, y, z); }
+        explicit inline operator Point3D<double>() const { return Point3D<double>(x, y, z); }
     };
 
 
