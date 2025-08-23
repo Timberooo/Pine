23773e722c4a407138bb3cb3aba122330c7c6eac
Timber <redwoodfox479@gmail.com>
2025-08-23 12:03:31 -0500

    Changed Point conversion operators to a single template function

--- src/math/point.h
+++ src/math/point.h
@@ -23,18 +23,8 @@ namespace Pine
         inline bool operator==(const Point2D<T>& other) const;
         inline bool operator!=(const Point2D<T>& other) const;
 
-        explicit inline operator Point2D<char>() const { return Point2D<char>(x, y); }
-        explicit inline operator Point2D<unsigned char>() const { return Point2D<unsigned char>(x, y); }
-        explicit inline operator Point2D<short>() const { return Point2D<short>(x, y); }
-        explicit inline operator Point2D<unsigned short>() const { return Point2D<unsigned short>(x, y); }
-        explicit inline operator Point2D<int>() const { return Point2D<int>(x, y); }
-        explicit inline operator Point2D<unsigned int>() const { return Point2D<unsigned int>(x, y); }
-        explicit inline operator Point2D<long>() const { return Point2D<long>(x, y); }
-        explicit inline operator Point2D<unsigned long>() const { return Point2D<unsigned long>(x, y); }
-        explicit inline operator Point2D<long long>() const { return Point2D<long long>(x, y); }
-        explicit inline operator Point2D<unsigned long long>() const { return Point2D<unsigned long long>(x, y); }
-        explicit inline operator Point2D<float>() const { return Point2D<float>(x, y); }
-        explicit inline operator Point2D<double>() const { return Point2D<double>(x, y); }
+        template <typename TReturn>
+        explicit inline operator Point2D<TReturn>() const { return Point2D<TReturn>(static_cast<TReturn>(x), static_cast<TReturn>(y)); }
     };
 
 
@@ -65,18 +55,8 @@ namespace Pine
         inline bool operator==(const Point3D<T>& other) const;
         inline bool operator!=(const Point3D<T>& other) const;
 
-        explicit inline operator Point3D<char>() const { return Point3D<char>(x, y, z); }
-        explicit inline operator Point3D<unsigned char>() const { return Point3D<unsigned char>(x, y, z); }
-        explicit inline operator Point3D<short>() const { return Point3D<short>(x, y, z); }
-        explicit inline operator Point3D<unsigned short>() const { return Point3D<unsigned short>(x, y, z); }
-        explicit inline operator Point3D<int>() const { return Point3D<int>(x, y, z); }
-        explicit inline operator Point3D<unsigned int>() const { return Point3D<unsigned int>(x, y, z); }
-        explicit inline operator Point3D<long>() const { return Point3D<long>(x, y, z); }
-        explicit inline operator Point3D<unsigned long>() const { return Point3D<unsigned long>(x, y, z); }
-        explicit inline operator Point3D<long long>() const { return Point3D<long long>(x, y, z); }
-        explicit inline operator Point3D<unsigned long long>() const { return Point3D<unsigned long long>(x, y, z); }
-        explicit inline operator Point3D<float>() const { return Point3D<float>(x, y, z); }
-        explicit inline operator Point3D<double>() const { return Point3D<double>(x, y, z); }
+        template <typename TReturn>
+        explicit inline operator Point3D<TReturn>() const { return Point3D<TReturn>(static_cast<TReturn>(x), static_cast<TReturn>(y), static_cast<TReturn>(z)); }
     };
 
 
