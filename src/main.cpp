96790f6b25d78e6a1f0b673881381e0d41d9508f
Timber <redwoodfox479@gmail.com>
2025-08-26 04:32:36 -0500

    Added default initializers to point structs and marked point constructors, operators, and functions as constexpr

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "io/terminal.h"
-#include "math.h"
-#include "callback.h"
+// #include "math.h"
+// #include "callback.h"
+
+#include "math/point.h"
 
 #include <string>
 
@@ -13,17 +15,35 @@ int testFunc()
 
 
 
+constexpr float test()
+{
+    Pine::Point2D<float> p1(0.5, 0);
+    Pine::Point2D<int> p2(1, 0);
+
+    return Pine::distance(p1, Pine::Point2D<float>(p2));
+}
+
+
+
 int main()
 {
     Pine::Terminal::setBufferSize(1024);
+
+    Pine::Terminal::writeLine(std::to_string(test()));
     
-    Pine::Point2D<int> p1(3, 2);
-    Pine::Point2D<int> p2(1, 5);
+    Pine::Point2D<float> p1(0, 0);
+    Pine::Point2D<int> p2(1, 1);
 
-    Pine::Vector2D v = p2 - p1;
+    Pine::Terminal::write("Enter p1's x value: ");
+    p1.x = std::stoi(Pine::Terminal::readLine());
+    Pine::Terminal::write("Enter p1's y value: ");
+    p1.y = std::stoi(Pine::Terminal::readLine());
+    Pine::Terminal::write("Enter p2's x value: ");
+    p2.x = std::stoi(Pine::Terminal::readLine());
+    Pine::Terminal::write("Enter p2's y value: ");
+    p2.y = std::stoi(Pine::Terminal::readLine());
 
-    Pine::Terminal::writeLine("distance: " + std::to_string(Pine::distance(p1, p2)));
-    Pine::Terminal::writeLine("v length: " + std::to_string(v.length()));
+    Pine::Terminal::writeLine("distance: " + std::to_string(Pine::distance(Pine::Point2D<int>(p1), p2)));
 
     Pine::Terminal::flush();
 
--- src/math/point.h
+++ src/math/point.h
@@ -16,40 +16,38 @@ namespace Pine
     // from p1 to p2, but these functions defer the float conversion to the end
     // of the calculation and are more obvious/intuitive
     template <typename T>
-    inline float distance(const Point2D<T>& p1, const Point2D<T>& p2);
+    constexpr inline float distance(const Point2D<T>& p1, const Point2D<T>& p2);
     template <typename T>
-    inline float distance(const Point3D<T>& p1, const Point3D<T>& p2);
+    constexpr inline float distance(const Point3D<T>& p1, const Point3D<T>& p2);
 
     // vvv Point2D vvv
 
     template <typename T>
     struct Point2D
     {
-        T x;
-        T y;
+        T x {};
+        T y {};
 
 
-        inline Point2D() {}
-        inline Point2D(T x, T y) : x(x), y(y) {}
+        constexpr inline Point2D() {}
+        constexpr inline Point2D(T x, T y) : x(x), y(y) {}
 
-        inline bool operator==(const Point2D<T>& other) const;
-        inline bool operator!=(const Point2D<T>& other) const;
+        constexpr inline bool operator==(const Point2D<T>& other) const;
+        constexpr inline bool operator!=(const Point2D<T>& other) const;
 
-        // Using template types so all supported conversions from T to TReturn
-        // can be used instead of only explicity supported conversions
         template <typename TReturn>
-        explicit inline operator Point2D<TReturn>() const { return Point2D<TReturn>(static_cast<TReturn>(x), static_cast<TReturn>(y)); }
+        explicit constexpr inline operator Point2D<TReturn>() const { return Point2D<TReturn>(static_cast<TReturn>(x), static_cast<TReturn>(y)); }
     };
 
 
 
     template <typename T>
-    inline bool Point2D<T>::operator==(const Point2D<T>& other) const {
+    constexpr inline bool Point2D<T>::operator==(const Point2D<T>& other) const {
         return x == other.x && y == other.y;
     }
 
     template <typename T>
-    inline bool Point2D<T>::operator!=(const Point2D<T>& other) const {
+    constexpr inline bool Point2D<T>::operator!=(const Point2D<T>& other) const {
         return !operator==(other);
     }
 
@@ -58,44 +56,39 @@ namespace Pine
     template <typename T>
     struct Point3D
     {
-        T x;
-        T y;
-        T z;
+        T x {};
+        T y {};
+        T z {};
 
 
-        inline Point3D() {}
-        inline Point3D(T x, T y, T z) : x(x), y(y), z(z) {}
+        constexpr inline Point3D() {}
+        constexpr inline Point3D(T x, T y, T z) : x(x), y(y), z(z) {}
 
-        inline bool operator==(const Point3D<T>& other) const;
-        inline bool operator!=(const Point3D<T>& other) const;
-
-        // Using template types so all supported conversions from T to TReturn
-        // can be used instead of only explicity supported conversions
-        template <typename TReturn>
-        explicit inline operator Point3D<TReturn>() const { return Point3D<TReturn>(static_cast<TReturn>(x), static_cast<TReturn>(y), static_cast<TReturn>(z)); }
+        constexpr inline bool operator==(const Point3D<T>& other) const;
+        constexpr inline bool operator!=(const Point3D<T>& other) const;
     };
 
 
 
     template <typename T>
-    inline bool Point3D<T>::operator==(const Point3D<T>& other) const {
+    constexpr inline bool Point3D<T>::operator==(const Point3D<T>& other) const {
         return x == other.x && y == other.y && z == other.z;
     }
 
     template <typename T>
-    inline bool Point3D<T>::operator!=(const Point3D<T>& other) const {
+    constexpr inline bool Point3D<T>::operator!=(const Point3D<T>& other) const {
         return !operator==(other);
     }
 
     // vvv Free Functions vvv // ^^^ Point3D ^^^
 
     template <typename T>
-    inline float distance(const Point2D<T>& p1, const Point2D<T>& p2) {
+    constexpr inline float distance(const Point2D<T>& p1, const Point2D<T>& p2) {
         return std::sqrt(static_cast<float>(square(p2.x - p1.x) + square(p2.y - p1.y)));
     }
 
     template <typename T>
-    inline float distance(const Point3D<T>& p1, const Point3D<T>& p2) {
+    constexpr inline float distance(const Point3D<T>& p1, const Point3D<T>& p2) {
         return std::sqrt(static_cast<float>(square(p2.x - p1.x) + square(p2.y - p1.y) + square(p2.z - p1.z)));
     }
 }
