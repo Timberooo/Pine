2119455434482892cab2a73a4a5b193cb9abdadb
Timber <redwoodfox479@gmail.com>
2025-08-26 04:45:07 -0500

    Marked size struct constructors and operators as constexpr

--- src/math/size.h
+++ src/math/size.h
@@ -17,27 +17,27 @@ namespace Pine
         T height {};
 
 
-        inline Size2D() {}
-        inline Size2D(T width, T height) : width(width), height(height) {}
+        constexpr inline Size2D() {}
+        constexpr inline Size2D(T width, T height) : width(width), height(height) {}
 
-        inline bool operator==(const Size2D<T>& other) const;
-        inline bool operator!=(const Size2D<T>& other) const;
+        constexpr inline bool operator==(const Size2D<T>& other) const;
+        constexpr inline bool operator!=(const Size2D<T>& other) const;
 
         // Using template types so all supported conversions from T to TReturn
         // can be used instead of only explicity supported conversions
         template <typename TReturn>
-        explicit inline operator Size2D<TReturn>() const { return Size2D<TReturn>(static_cast<TReturn>(width), static_cast<TReturn>(height)); }
+        explicit constexpr inline operator Size2D<TReturn>() const { return Size2D<TReturn>(static_cast<TReturn>(width), static_cast<TReturn>(height)); }
     };
 
 
 
     template <typename T>
-    inline bool Size2D<T>::operator==(const Size2D<T>& other) const {
+    constexpr inline bool Size2D<T>::operator==(const Size2D<T>& other) const {
         return width == other.width && height == other.height;
     }
 
     template <typename T>
-    inline bool Size2D<T>::operator!=(const Size2D<T>& other) const {
+    constexpr inline bool Size2D<T>::operator!=(const Size2D<T>& other) const {
         return !operator==(other);
     }
 
@@ -51,27 +51,27 @@ namespace Pine
         T length {};
 
 
-        inline Size3D() {}
-        inline Size3D(T width, T height, T length) : width(width), height(height), length(length) {}
+        constexpr inline Size3D() {}
+        constexpr inline Size3D(T width, T height, T length) : width(width), height(height), length(length) {}
 
-        inline bool operator==(const Size3D<T>& other) const;
-        inline bool operator!=(const Size3D<T>& other) const;
+        constexpr inline bool operator==(const Size3D<T>& other) const;
+        constexpr inline bool operator!=(const Size3D<T>& other) const;
 
         // Using template types so all supported conversions from T to TReturn
         // can be used instead of only explicity supported conversions
         template <typename TReturn>
-        explicit inline operator Size3D<TReturn>() const { return Size3D<TReturn>(static_cast<TReturn>(width), static_cast<TReturn>(height), static_cast<TReturn>(length)); }
+        explicit constexpr inline operator Size3D<TReturn>() const { return Size3D<TReturn>(static_cast<TReturn>(width), static_cast<TReturn>(height), static_cast<TReturn>(length)); }
     };
 
 
 
     template <typename T>
-    inline bool Size3D<T>::operator==(const Size3D<T>& other) const {
+    constexpr inline bool Size3D<T>::operator==(const Size3D<T>& other) const {
         return width == other.width && height == other.height && length == other.length;
     }
 
     template <typename T>
-    inline bool Size3D<T>::operator!=(const Size3D<T>& other) const {
+    constexpr inline bool Size3D<T>::operator!=(const Size3D<T>& other) const {
         return !operator==(other);
     }
 }
