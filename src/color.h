09cc7d63063b695f6e5a0646452e9c335a82d70d
Timber <redwoodfox479@gmail.com>
2025-09-21 14:54:16 -0500

    Removed note from color.h

--- src/color.h
+++ src/color.h
@@ -1,10 +1,3 @@
-/* * * * * * * * * * * * * * * * * * NOTES * * * * * * * * * * * * * * * * * *\
- * - The lookup table for 4 bit colors will need to support custom color     *
- *   palettes since each console has a unique palette. Make sure to add      *
- *   palette presets for known palettes and an option for a custom palette   *
- *   for any that're missed/not covered.                                     *
-\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 #ifndef PINE_COLOR_H
 #define PINE_COLOR_H
 
